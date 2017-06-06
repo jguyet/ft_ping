084220365796a12f863ecab4270d490bf7298f08
Jeremy GUYET <jguyet@e1r12p5.42.fr>
2017-06-06 05:07:37 +0200

    flux

--- srcs/utils/get_str_inet_addr.c
+++ srcs/utils/get_str_inet_addr.c
@@ -39,7 +39,7 @@ struct sockaddr_in *get_sockaddr_in_ipv4(char *host)
 
 	ft_memset(&hints, 0, sizeof(hints));
 	hints.ai_family = PROT_INTERNET_IPV4;
-	hints.ai_socktype = SOCK_DGRAM;
+	hints.ai_socktype = NETWORK_FLUX;
 	if (getaddrinfo(host, NULL, &hints, &res) != 0)
 		return (NULL);
 	while (res)
